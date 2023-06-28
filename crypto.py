4fe9ff655e234a7d60c4cb75da2f3e9690553090
Dawid Ciepiela <zk791167@gmail.com>
2023-06-28 17:26:28 +0200

    commiting recent changes and abandoning project

---
+++ crypto.py
@@ -0,0 +1,46 @@
+import os
+import hashlib
+import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+_BLOCK_SIZE = 16
+
+class AesCipher:
+    def __init__(self, key):
+        self.key = hashlib.sha256(os.urandom(_BLOCK_SIZE)).digest()
+
+    def encrypt_str(self, raw:str) -> bytes:
+        iv = os.urandom(_BLOCK_SIZE)
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), default_backend())
+        encryptor = cipher.encryptor()
+        raw = self._pad(raw)
+        return iv + encryptor.update(raw) + encryptor.finalize()
+
+    def decrypt_str(self, enc:bytes) -> str:
+        iv = enc[:_BLOCK_SIZE]
+        enc = enc[_BLOCK_SIZE:]
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), default_backend())
+        decryptor = cipher.decryptor()
+        raw = decryptor.update(enc) + decryptor.finalize()
+        return self._unpad(raw)
+    
+    @staticmethod
+    def _pad(s:bytes) -> bytes:
+        padding = (_BLOCK_SIZE - (len(s) % _BLOCK_SIZE))
+        return s + (padding * chr(padding)).encode()
+    
+    @staticmethod
+    def _unpad(s:bytes) -> bytes:
+        return s[:-ord(s[len(s)-1:])]
+
+
+if __name__ == '__main__':
+    cipher = AesCipher('my secret password')
+    print(cipher.key)
+
+    enc_msg = base64.urlsafe_b64encode(cipher.encrypt_str(cipher.key))
+    print(enc_msg)
+
+    dec_msg = cipher.decrypt_str(base64.urlsafe_b64decode(enc_msg))
+    print(dec_msg)
--- portal.py
+++ portal.py
@@ -1,5 +1,6 @@
 import os
 import re
+import random
 from typing import Union, Optional
 from bs4 import BeautifulSoup
 from urllib.parse import urlencode
@@ -13,6 +14,9 @@ SCHULPORTAL_SCHOOL_ID = os.environ.get("SCHULPORTAL_SCHOOL_ID", "5114")
 SCHULPORTAL_START_URL = os.environ["SCHULPORTAL_START_URL"]
 SCHULPORTAL_LOGIN_URL = os.environ["SCHULPORTAL_LOGIN_URL"]
 SCHULPORTAL_VERTRETUNGSPLAN_URL = os.environ["SCHULPORTAL_VERTRETUNGSPLAN_URL"]
+SCHULPORTAL_MEINUNTERRICHT_URL = os.environ["SCHULPORTAL_MEINUNTERRICHT_URL"]
+SCHULPORTAL_NACHRICHTEN_URL = os.environ["SCHULPORTAL_NACHRICHTEN_URL"]
+SCHULPORTAL_AJAX_URL = os.environ["SCHULPORTAL_AJAX_URL"]
 
 def getSoup(blob: Union[str, bytes]) -> BeautifulSoup:
     """
@@ -43,7 +47,6 @@ def buildArgs(blob: str) -> list[str]:
             re.compile("([^!.?]+[!.?])\s").split(" ".join(map(lambda x: str(x).strip(), blob.splitlines())))
         )
     )
-
 
 class Portal:
     def __init__(self, username: str, password: str, *, session: Optional[ClientSession]=None):
@@ -138,6 +141,73 @@ class Portal:
             # TODO: Implement
             raise NotImplemented(self.check_substitutes)
 
+    async def get_undone_homework(self):
+        """
+        Retrieves a list of undone homework from a web portal.
+
+        Raises:
+            NotSignedIn: If the user is not signed in.
+
+        Returns:
+            A list of dictionaries representing undone homework tasks. Each dictionary contains the following keys:
+                - "topic": The topic or title of the homework task.
+                - "teacher": The name of the teacher associated with the homework task.
+                - "date": The date of the homework task.
+                - "content": The content or description of the homework task.
+        """
+        if not self._loggedIn:
+            raise NotSignedIn("Sign in first.")
+
+        async with self._session.get(SCHULPORTAL_MEINUNTERRICHT_URL) as response:
+            content = await response.text()
+            soup = getSoup(content)
+            tasks = []
+            for el in soup.find_all("tr", attrs={"class":"printable"}):
+                if el.find(attrs={"class":"undone"}) is None:
+                    continue
+                subject = el.select_one('span[class="name"]')
+                teacher = el.select_one('span.teacher > div.btn-group > button')
+                topic = el.select_one('b[class="thema"]')
+                date = el.select_one('span[class="datum"]')
+                content = el.select_one('div.realHomework')
+                task = {}
+                if not subject is None:
+                    task["topic"] = topic.extract().getText().strip()
+                if not teacher is None:
+                    task["teacher"] = teacher.extract().get("title", "").strip()
+                if not topic is None:
+                    task["topic"] = topic.extract().getText().strip()
+                if not date is None:
+                    task["date"] = date.extract().getText().strip()
+                if not content is None:
+                    task["content"] = content.extract().getText().strip()
+                tasks.append(task)
+
+            return tasks
+
+    async def spoof_messages(self):
+        if not self._loggedIn:
+            raise NotSignedIn("Sign in first.")
+        await self._session.get(SCHULPORTAL_NACHRICHTEN_URL)
+        # requires RSA handshake and decrption:
+        # GET ajax.php?f=rsaPublicKey
+        # POST ajax.php?f=rsaHandshake&s={randint(0,2000)} {key:AES128 key self encrypted}
+        async with self._session.post(
+            SCHULPORTAL_NACHRICHTEN_URL,
+            data=urlencode({
+                "a": "headers",
+                "getType": "visibleOnly",
+                "last": "0"
+            }),
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        ) as response:
+            content = await response.text()
+            print(content)
+
+
     async def list(self) -> list[dict[str, str]]:
         """
         Get a list of schools from the portal.
@@ -222,4 +292,8 @@ async def main():
             await portal.check_substitutes()
         except NothingToReturn as ex:
             print(*ex.args)
+        tasks = await portal.get_undone_homework()
+        print(tasks)
+
+        await portal.spoof_messages()
         await portal.logout()
